51e9e0b965fa4252430757e6f2a3c44b5cfe1da6
Kauan Pablo <kauapablo089@gmail.com>
2024-01-13 15:47:16 -0300

    Add submissão

---
+++ 2 - Ad-Hoc/1030 - A Lenda de Flavious Josephus.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int josephus(int n, int k) {
+    if (n == 1) {
+        return 1;
+    }
+    return (josephus(n - 1, k) + k - 1) % n + 1;
+}
+
+int main() {
+    int nc;
+    cin >> nc;
+
+    for (int i = 1; i <= nc; i++) {
+        int n, k;
+        cin >> n >> k;
+        cout << "Case " << i << ": " << josephus(n, k) << endl;
+    }
+
+    return 0;
+}
